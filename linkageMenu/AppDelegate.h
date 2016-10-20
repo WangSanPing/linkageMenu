f7aad4b7c46c4e39f04bc01d04633f644a260cb4
WangSanPing <wangxu_1987@126.com>
2016-10-20 14:06:44 +0800

    2016-10-20级联菜单
    2016-10-20级联菜单

---
+++ linkageMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  linkageMenu
+//
+//  Created by 王旭 on 2016/10/20.
+//  Copyright © 2016年 wangxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ linkageMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  linkageMenu
+//
+//  Created by 王旭 on 2016/10/20.
+//  Copyright © 2016年 wangxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
